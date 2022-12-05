664b02b905c13c7af22dc22616efa2b55a43064b
02njani <113149365+02njani@users.noreply.github.com>
2022-12-05 08:23:47 -0500

    Add files via upload

---
+++ LinearProbingHashMap.java
@@ -0,0 +1,433 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.NoSuchElementException;
+
+/**
+ * Your implementation of a LinearProbingHashMap.
+ *
+ * @author Nitya Jani
+ * @version 1.0
+ * @userid njani8
+ * @GTID 903598748
+ *
+ * Collaborators: LIST ALL COLLABORATORS YOU WORKED WITH HERE
+ *
+ * Resources: LIST ALL NON-COURSE RESOURCES YOU CONSULTED HERE
+ */
+public class LinearProbingHashMap<K, V> {
+
+    /**
+     * The initial capacity of the LinearProbingHashMap when created with the
+     * default constructor.
+     *
+     * DO NOT MODIFY THIS VARIABLE!
+     */
+    public static final int INITIAL_CAPACITY = 13;
+
+    /**
+     * The max load factor of the LinearProbingHashMap
+     *
+     * DO NOT MODIFY THIS VARIABLE!
+     */
+    public static final double MAX_LOAD_FACTOR = 0.67;
+
+    // Do not add new instance variables or modify existing ones.
+    private LinearProbingMapEntry<K, V>[] table;
+    private int size;
+
+    /**
+     * Constructs a new LinearProbingHashMap.
+     *
+     * The backing array should have an initial capacity of INITIAL_CAPACITY.
+     *
+     * Use constructor chaining.
+     */
+    public LinearProbingHashMap() {
+        this(INITIAL_CAPACITY);
+    }
+
+    /**
+     * Constructs a new LinearProbingHashMap.
+     *
+     * The backing array should have an initial capacity of initialCapacity.
+     *
+     * You may assume initialCapacity will always be positive.
+     *
+     * @param initialCapacity the initial capacity of the backing array
+     */
+    public LinearProbingHashMap(int initialCapacity) {
+        this.table = new LinearProbingMapEntry[initialCapacity];
+    }
+
+    /**
+     * Adds the given key-value pair to the map. If an entry in the map
+     * already has this key, replace the entry's value with the new one
+     * passed in.
+     *
+     * In the case of a collision, use linear probing as your resolution
+     * strategy.
+     *
+     * Before actually adding any data to the HashMap, you should check to
+     * see if the array would violate the max load factor if the data was
+     * added. For example, let's say the array is of length 5 and the current
+     * size is 3 (LF = 0.6). For this example, assume that no elements are
+     * removed in between steps. If another entry is attempted to be added,
+     * before doing anything else, you should check whether (3 + 1) / 5 = 0.8
+     * is larger than the max LF. It is, so you would trigger a resize before
+     * you even attempt to add the data or figure out if it's a duplicate. Be
+     * careful to consider the differences between integer and double
+     * division when calculating load factor.
+     *
+     * When regrowing, resize the length of the backing table to
+     * 2 * old length + 1. You must use the resizeBackingTable method to do so.
+     *
+     * Return null if the key was not already in the map. If it was in the map,
+     * return the old value associated with it.
+     *
+     * @param key   the key to add
+     * @param value the value to add
+     * @return null if the key was not already in the map. If it was in the
+     * map, return the old value associated with it
+     * @throws java.lang.IllegalArgumentException if key or value is null
+     */
+    public V put(K key, V value) {
+        if (key == null || value == null) {
+            throw new IllegalArgumentException("The key or value is null so the pair can't be added.");
+        }
+        int index = Math.abs(key.hashCode() % table.length);
+        double nextSize = ((double) (size + 1) / (double) table.length);
+        if (nextSize >= MAX_LOAD_FACTOR) {
+            resizeBackingTable(2 * table.length + 1);
+            index = Math.abs(key.hashCode() % table.length);
+        }
+        int firstDEL = -1;
+        int counter = 0;
+        V data = null;
+        boolean reachedANull = false;
+        if (size == 0) {
+            table[index] = new LinearProbingMapEntry<>(key, value);
+        }
+        while (counter < size) {
+            if (table[index] == null) {
+                reachedANull = true;
+                if (firstDEL == -1) {
+                    table[index] = new LinearProbingMapEntry<>(key, value);
+                } else {
+                    table[firstDEL].setKey(key);
+                    table[firstDEL].setValue(value);
+                }
+                counter++;
+                data = null;
+                break;
+            } else if (table[index].getKey().equals(key)) {
+                if (!table[index].isRemoved()) {
+                    //if it's not removed, reset it
+                    data = table[index].getValue();
+                    table[index].setValue(value);
+                } else {
+                    //it is removed so replace the first DEL
+                    if (firstDEL == -1) {
+                        //this means the first del is this specific entry
+                        firstDEL = index;
+                    }
+                    table[firstDEL].setKey(key);
+                    table[firstDEL].setValue(value);
+                    data = null;
+                }
+                counter++;
+                break;
+            } else if (table[index].isRemoved()) {
+                if (firstDEL == -1) {
+                    firstDEL = index;
+                }
+                index = (index + 1) % table.length;
+            }
+            //counter++;
+        }
+        //if it never reached a null
+        if (!reachedANull) {
+            if (firstDEL != -1) {
+                table[firstDEL].setKey(key);
+                table[firstDEL].setValue(value);
+                data = null;
+            }
+        }
+        size = size + 1;
+        return data;
+    }
+
+    /**
+     * Removes the entry with a matching key from map by marking the entry as
+     * removed.
+     *
+     * @param key the key to remove
+     * @return the value previously associated with the key
+     * @throws java.lang.IllegalArgumentException if key is null
+     * @throws java.util.NoSuchElementException   if the key is not in the map
+     */
+    public V remove(K key) {
+        if (key == null) {
+            throw new IllegalArgumentException("The key is nothing so you can't remove it.");
+        }
+        if (size == 0) {
+            throw new NoSuchElementException("The map is empty so the key can't be in the list.");
+        }
+        int index = Math.abs(key.hashCode() % table.length);
+        int counter = 0;
+        V value = null;
+        while (counter < size) {
+            if (table[index] == null) {
+                throw new NoSuchElementException("The key is not in the table.");
+            } else if (table[index].getKey().equals(key)) {
+                if (!table[index].isRemoved()) {
+                    value = table[index].getValue();
+                    table[index].setRemoved(true);
+                    counter++;
+                    break;
+                } else {
+                    throw new NoSuchElementException("The key is not in the list.");
+                }
+            } else if (table[index].isRemoved()) {
+                index = (index + 1) % table.length;
+            } else {
+                index = (index + 1) % table.length;
+                counter++;
+            }
+        }
+        //it went through size traversals of the list but the value didn't change (which means it wasn't found)
+        if (value == null) {
+            throw new NoSuchElementException("The key is not in the list.");
+        }
+        size = size - 1;
+        return value;
+    }
+
+    /**
+     * Gets the value associated with the given key.
+     *
+     * @param key the key to search for in the map
+     * @return the value associated with the given key
+     * @throws java.lang.IllegalArgumentException if key is null
+     * @throws java.util.NoSuchElementException   if the key is not in the map
+     */
+    public V get(K key) {
+        if (key == null) {
+            throw new IllegalArgumentException("The key is null so you can't get anything.");
+        }
+        int index = Math.abs(key.hashCode() % table.length);
+        int counter = 0;
+        V value = null;
+        while (counter < size) {
+            if (table[index] == null) {
+                throw new NoSuchElementException("The key is not in the table.");
+            } else if (table[index].getKey().equals(key)) {
+                if (!table[index].isRemoved()) {
+                    value = table[index].getValue();
+                    counter++;
+                    break;
+                } else {
+                    throw new NoSuchElementException("The key is not in the list.");
+                }
+            } else if (table[index].isRemoved()) {
+                index = (index + 1) % table.length;
+            } else {
+                index = (index + 1) % table.length;
+                counter++;
+            }
+
+        }
+        if (value == null) {
+            throw new NoSuchElementException("The key is not in the list.");
+        }
+        return value;
+    }
+
+    /**
+     * Returns whether the key is in the map or not.
+     *
+     * @param key the key to search for in the map
+     * @return true if the key is contained within the map, false
+     * otherwise
+     * @throws java.lang.IllegalArgumentException if key is null
+     */
+    public boolean containsKey(K key) {
+        if (key == null) {
+            throw new IllegalArgumentException("The key is null so it can't be searched for.");
+        }
+        int index = Math.abs(key.hashCode() % table.length);
+        int counter = 0;
+        boolean answer = false;
+        while (counter < size) {
+            if (table[index] == null) {
+                answer = false;
+                break;
+            } else if (table[index].getKey().equals(key)) {
+                if (!table[index].isRemoved()) {
+                    answer = true;
+                    counter++;
+                } else {
+                    answer = false;
+                }
+                break;
+            } else if (table[index].isRemoved()) {
+                index = (index + 1) % table.length;
+            } else {
+                index = (index + 1) % table.length;
+                counter++;
+            }
+
+        }
+        return answer;
+    }
+
+    /**
+     * Returns a Set view of the keys contained in this map.
+     *
+     * Use java.util.HashSet.
+     *
+     * @return the set of keys in this map
+     */
+    public Set<K> keySet() {
+        int counter = 0;
+        Set<K> keys = new HashSet<>();
+        for (int i = 0; i < table.length; i++) {
+            if (table[i] != null && !table[i].isRemoved()) {
+                keys.add(table[i].getKey());
+            }
+            if (counter == size) {
+                break;
+            }
+            counter++;
+        }
+        return keys;
+    }
+
+    /**
+     * Returns a List view of the values contained in this map.
+     *
+     * Use java.util.ArrayList or java.util.LinkedList.
+     *
+     * You should iterate over the table in order of increasing index and add
+     * entries to the List in the order in which they are traversed.
+     *
+     * @return list of values in this map
+     */
+    public List<V> values() {
+        int counter = 0;
+        List<V> values = new ArrayList<>();
+        for (int i = 0; i < table.length; i++) {
+            if (table[i] != null && !table[i].isRemoved()) {
+                values.add(table[i].getValue());
+            }
+            if (counter == size) {
+                break;
+            }
+            counter++;
+        }
+        return values;
+    }
+
+    /**
+     * Resize the backing table to length.
+     *
+     * Disregard the load factor for this method. So, if the passed in length is
+     * smaller than the current capacity, and this new length causes the table's
+     * load factor to exceed MAX_LOAD_FACTOR, you should still resize the table
+     * to the specified length and leave it at that capacity.
+     *
+     * You should iterate over the old table in order of increasing index and
+     * add entries to the new table in the order in which they are traversed. 
+     * You should NOT copy over removed elements to the resized backing table.
+     *
+     * Since resizing the backing table is working with the non-duplicate
+     * data already in the table, you shouldn't explicitly check for
+     * duplicates.
+     *
+     * Hint: You cannot just simply copy the entries over to the new array.
+     *
+     * @param length new length of the backing table
+     * @throws java.lang.IllegalArgumentException if length is less than the
+     *                                            number of items in the hash
+     *                                            map
+     */
+    public void resizeBackingTable(int length) {
+        if (length < size) {
+            throw new IllegalArgumentException("The new table can't fit all of the old data.");
+        }
+        LinearProbingMapEntry<K, V>[] tempTable = new LinearProbingMapEntry[length];
+        //go through the old table, rehash all the items
+        for (int i = 0; i < table.length; i++) {
+            if (table[i] != null && !table[i].isRemoved()) {
+                int index = Math.abs(table[i].getKey().hashCode() % tempTable.length);
+                int counter = 0;
+                while (counter < size) {
+                    if (tempTable[index] == null) {
+                        tempTable[index] = new LinearProbingMapEntry<>(table[i].getKey(), table[i].getValue());
+                        break;
+                    } else {
+                        index = (index + 1) % tempTable.length;
+                    }
+                    counter++;
+                }
+            }
+        }
+        table = tempTable;
+    }
+
+    /**
+     * Clears the map.
+     *
+     * Resets the table to a new array of the INITIAL_CAPACITY and resets the
+     * size.
+     *
+     * Must be O(1).
+     */
+    public void clear() {
+        table = new LinearProbingMapEntry[INITIAL_CAPACITY];
+        size = 0;
+    }
+
+    /**
+     * Returns the table of the map.
+     *
+     * For grading purposes only. You shouldn't need to use this method since
+     * you have direct access to the variable.
+     *
+     * @return the table of the map
+     */
+    public LinearProbingMapEntry<K, V>[] getTable() {
+        // DO NOT MODIFY THIS METHOD!
+        return table;
+    }
+
+    /**
+     * Returns the size of the map.
+     *
+     * For grading purposes only. You shouldn't need to use this method since
+     * you have direct access to the variable.
+     *
+     * @return the size of the map
+     */
+    public int size() {
+        // DO NOT MODIFY THIS METHOD!
+        return size;
+    }
+
+    /*public static void main(String[] args) {
+        LinearProbingHashMap<Integer, Integer> map = new LinearProbingHashMap<>(5);
+        map.put(2, 20);
+        map.put(15, 21);
+        map.put(30, 12);
+        map.put(19, 38);
+        map.put(18, 57);
+        map.remove(2);
+        map.remove(30);
+        map.remove(19);
+        map.remove(18);
+        map.put(19, 22);
+        Integer g = map.get(19);
+        System.out.println(g);
+    }*/
+}
